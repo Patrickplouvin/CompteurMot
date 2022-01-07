5d52346ad991f1eccbc250ad0cfe11a2e404f9ae
Plouvin Patrick <patrick_plouvin@ens.univ-artois.fr>
2022-01-07 17:13:26 +0100

    recc 2

--- Recurrence.py
+++ Recurrence.py
@@ -1,6 +1,13 @@
-import compteurMot
+from CompteurMot import strToArray
+import CompteurMot
 
 
 
 def recurrence(message):
+    tab=strToArray(message," ")
+    print(tab.count("."))
     
+    print(tab)
+    
+        
+recurrence('coucou la tiktokerie.')
