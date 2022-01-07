bde433fb2ac235a6578bc22a895091af018b4da2
Patrickplouvin <Patrick.Plouvinpro@gmail.com>
2022-01-07 18:34:57 +0100

    Occurence terminé

---
+++ Compteur.py
@@ -0,0 +1,12 @@
+import CompteurMot
+import Recurrence
+"""import CompteurPhrase"""
+
+
+
+message=input("Rentrez votre flux:"  )
+liste=input("Rentrez la liste des mots dont vous souhaitez voir l'occurence :" )
+
+print(Recurrence.rec(message,liste))
+
+
--- Recurrence.py
+++ Recurrence.py
@@ -1,6 +1,10 @@
-import compteurMot
+import CompteurMot
+
+def rec(message,liste):
+    occ=[]
+    liste=CompteurMot.strToArray(liste," ")
+    message=CompteurMot.strToArray(message," |\. |\! |\? |\; |\, |\: ")
+    for i in liste:
+        print("le mot "+i+" est "+str(message.count(i))+" fois")
 
 
-
-def recurrence(message):
-    
