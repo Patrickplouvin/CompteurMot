3aa94cf69e220aae643a06591ac5d72bc7105b9b
Delsol Cameron <cameron_delsol@ens.univ-artois.fr>
2022-01-07 16:50:00 +0100

    changement strToArray

--- CompteurMot.py
+++ CompteurMot.py
@@ -1,6 +1,5 @@
-def strToArray(str : str):
-    separator = ""
-    return str.split(separator)
+def strToArray(str : str,seperator):
+    return str.split(seperator)
 
 def nbMots(tab):
     return len(tab)
